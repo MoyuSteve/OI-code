ae503e1d15f9c65cfd91fe71e3d75e78fb002d51
MoyuSteve <Lhy1837972338@outlook.com>
2020-03-22 21:59:11 +0800

    缩进

--- OIcode.cpp
+++ OIcode.cpp
@@ -6,26 +6,26 @@ int c[N][N];
 int s[N][N];
 signed main()
 {
-    int T,k;
-    scanf("%lld%lld",&T,&k);
-    for(int i = 0;i < N;i++)
-        for(int j = 0;j<=i;j++)
-        {
-            if (!j)c[i][j] = 1;
-            else c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % k;
-            if(!c[i][j]) s[i][j] = 1;
-        }
-    for(int i = 0;i < N;i++)
-        for(int j = 0;j < N;j++)
-        {
-            if(i)s[i][j] += s[i-1][j];
-            if(j)s[i][j] += s[i][j-1];
-            if(i&&j)s[i][j] -= s[i-1][j-1];
-        }
-    while(T--)
-    {
-        int n,m;
-        scanf("%lld%lld",&n,&m);
-        printf("%lld",s[n][m]);
-    }
+	int T,k;
+	scanf("%lld%lld",&T,&k);
+	for(int i = 0;i < N;i++)
+		for(int j = 0;j<=i;j++)
+		{
+			if (!j)c[i][j] = 1;
+			else c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % k;
+			if(!c[i][j]) s[i][j] = 1;
+		}
+	for(int i = 0;i < N;i++)
+		for(int j = 0;j < N;j++)
+		{
+			if(i)s[i][j] += s[i-1][j];
+			if(j)s[i][j] += s[i][j-1];
+			if(i&&j)s[i][j] -= s[i-1][j-1];
+		}
+	while(T--)
+	{
+		int n,m;
+		scanf("%lld%lld",&n,&m);
+		printf("%lld",s[n][m]);
+	}
 }
