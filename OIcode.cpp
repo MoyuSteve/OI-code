6a489c590a96975e6e91d6eeb08fe10f4bd66842
MoyuSteve <Lhy1837972338@outlook.com>
2020-03-22 20:12:55 +0800

    Hank计算系数

--- OIcode.cpp
+++ OIcode.cpp
@@ -1,84 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-
-#include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <cstdlib>
+#include<iostream>
+#include<algorithm>
+#define int long long
 using namespace std;
-const int N = 50010;
-int primes[N], cnt;
-bool st[N];
-
-
-using ll = long long;
-using PII = pair<int, int>;
-PII factor[N];
-int divider[N];
-int cntf, cntd;
-
-int gcd(int a, int b)
+const int mod = 10007;
+int pow(int a, int x)
 {
-	return b ? gcd(b, a % b) : a;
+	int r = 1;
+	while (x)
+	{
+		if (x & 1)r = r * a % mod;
+		a = a * a % mod; x >>= 1;
+	}
+	return r;
 }
-
-void get_primes(int n)
+signed main(void)
 {
-	for (int i = 2; i <= n; i++)
-	{
-		if (!st[i])primes[cnt++] = i;
-		for(int j = 0;primes[j] <= n/i;j++)
-		{
-			st[i * primes[j]] = true;
-			if (!(i % primes[j]))break;
-		}
-	}
-}
-void dfs(int u, int p)
-{
-	if (u == cntf)
-	{
-		divider[cntd++] = p;
-		return;
-	}
-	for (int i = 0; i <= factor[u].second; i++)
-	{
-		dfs(u + 1, p);
-		p *= factor[u].first;
-	}
-}
-int main()
-{
-	get_primes(N - 1);
-	int n;
-	scanf("%d", &n);
-	while (n--)
-	{
-		int a0, a1, b0, b1;
-		scanf("%d%d%d%d", &a0, &a1, &b0, &b1);
-		cntf = 0;
-		int d = b1;
-		for (int i = 0; primes[i] <= d / primes[i]; i++)
-		{
-			int p = primes[i];
-			if (!(d % p))
-			{
-				int s = 0;
-				while (!(d % p))
-					d /= p, s++;
-				factor[cntf++] = make_pair(p, s);
-
-			}
-		}
-		if (d > 1)factor[cntf++] = make_pair(d, 1);
-		cntd = 0;
-		dfs(0, 1);
-		int res = 0;
-		for (int i = 0; i < cntd; i++)
-		{
-			int x = divider[i];
-			if (gcd(x, a0) == a1 && (ll)x / gcd(x, b0) * b0 == b1)res ++;
-		}
-		printf("%d\n", res);
-	}
+	int a, b, k, n, m;
+	cin >> a >> b >> k >> n >> m;
+	int r = pow(a, n) * pow(b, m) % mod;
+	for (int i = 1, j = k; i <= n; i++, j--)
+		r = r * j % mod, r = r * pow(i, mod - 2) % mod;
+	cout << r;
+	return 0;
 }
